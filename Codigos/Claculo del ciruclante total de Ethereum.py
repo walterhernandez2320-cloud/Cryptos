221b02350795762e4188b2630514a57a6378b1d4
walterhernandez2320-cloud <walterhernandez.2320@gmail.com>
2025-11-20 00:28:26 -0300

    Add files via upload

---
+++ Codigos/Claculo del ciruclante total de Ethereum.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv("cryptos_unidas.csv")
+
+eth = df[df["Name"] == "Ethereum"]
+eth["Date"] = pd.to_datetime(eth["Date"])
+eth = eth.sort_values("Date")
+
+ultimo = eth.iloc[-1] # aca se buscan los datos del ultimo dia del CSV que tiene informacion de Ethereum
+circulante = ultimo["Marketcap"] / ultimo["Close"]
+
+print("Fecha:", ultimo["Date"])
+print("Marketcap:", ultimo["Marketcap"])
+print("Precio (Close):", ultimo["Close"])
+print("Circulante total:", circulante)
